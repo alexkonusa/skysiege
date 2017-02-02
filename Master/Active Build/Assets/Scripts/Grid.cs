0d21110c25b39275dccbcbf6b3c2636027f059b0
alexkonusa <alex.konusa@hotmail.co.uk>
2017-02-02 19:17:52 +0000

    Active Build

---
+++ Master/Active Build/Assets/Scripts/Grid.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour 
+{
+
+	public Color mouseOverColor;
+	public GameObject currBuiltObject;
+
+	Renderer _renderer;
+	private Color gridStartColor;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+		_renderer = GetComponent<Renderer> ();
+		gridStartColor = _renderer.material.color;
+	
+	}
+
+	void OnMouseDown ()
+	{
+
+		if (currBuiltObject == null) {
+
+			Debug.Log ("It cant be built");
+			return;
+
+		} 
+		else 
+		{
+
+			//Deploy the ship
+
+		}
+	
+	}
+
+	void OnMouseEnter ()
+	{
+
+		_renderer.material.color = mouseOverColor;
+			
+	}
+
+	void OnMouseExit ()
+	{
+
+		_renderer.material.color = gridStartColor;
+
+	}
+}
---
+++ Master/Active Build/Assets/Scripts/ShipInventory_Click.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShipInventory_Click : MonoBehaviour 
+{
+
+	public int id = 1;
+	public int avalible_Ships = 1;
+	public Text avalibleShip_Display;
+
+	GameObject objectsParent;
+	// Use this for initialization
+	void Start () 
+	{
+
+		this.gameObject.name = "Ship_" + id;
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+		avalibleShip_Display.text = "" + avalible_Ships;
+	
+	}
+
+	public void OnMouseClick()
+	{
+
+		if (avalible_Ships > 0) {
+
+				Debug.Log ("You have ships");
+				avalible_Ships -- ;
+
+			} 
+			else 
+			{
+
+				Debug.Log ("You Don't have any ships");
+			this.gameObject.SetActive (false);
+			}
+	}
+}
