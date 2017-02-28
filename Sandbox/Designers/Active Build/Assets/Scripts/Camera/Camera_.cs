93b9fa84e0c2a56cf792dff254d2e17087c296a3
alexkonusa <alex.konusa@hotmail.co.uk>
2017-02-28 14:40:00 +0000

    dgdfgdfgdfg

---
+++ Sandbox/Designers/Active Build/Assets/Scripts/Camera/Camera_.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_ : MonoBehaviour 
+{
+
+	//Camera target
+	public Transform target;
+	//Rotation Speed
+	public float rotationSpeed = 10;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+		
+	void LateUpdate () 
+	{
+		
+		transform.LookAt (target);
+
+		if (Input.GetKey ("left")) 
+		{
+			transform.Translate (Vector3.left * Time.deltaTime);
+		}
+
+		if (Input.GetKey ("right")) 
+		{
+			transform.Translate (Vector3.right * Time.deltaTime);
+		}
+
+	}
+}
---
+++ Sandbox/Designers/Active Build/Assets/Scripts/UI/UI_BuildingSlot.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UI_BuildingSlot : MonoBehaviour 
+{
+	//Now we can use this GameObject to find the picture, description and any other 
+	//information we need
+	public GameObject this_Building;
+	public Image icon;
+	public Text description;
+
+	void Start ()
+	{
+
+		icon.sprite = this_Building.GetComponent<Hanger>().icon;
+		description.text = this_Building.GetComponent<Hanger>().description;
+
+	}
+
+}
