d5fb180dfecc26e857e4b10b8399a11ba63f3b13
alexkonusa <alex.konusa@hotmail.co.uk>
2017-03-07 12:44:39 +0000

    test

---
+++ Master/Active Build/Assets/Scripts/AI/AI_Ally.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI_Ally : MonoBehaviour
+{
+
+    public float health;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (health <= 0)
+        {
+
+            Destroy(gameObject);
+
+        }
+	
+	}
+}
