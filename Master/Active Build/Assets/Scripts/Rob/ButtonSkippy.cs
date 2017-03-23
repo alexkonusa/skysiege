3696571b9814f97061eaa1a47c1434b011ff6ed5
Rayner <robrayner96@hotmail.com>
2017-03-23 16:16:39 +0000

    Button script for skipping between waves

---
+++ Master/Active Build/Assets/Scripts/Rob/ButtonSkippy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonSkippy : MonoBehaviour {
+
+	public void SkipWait () {
+		//Set Wave to be eligible to start
+		GameObject.Find ("GameManagers").GetComponent<WaveMachine> ().startWave = false;
+		// Sets the timer to 5 seconds
+		GameObject.Find ("GameManagers").GetComponent<WaveMachine> ().startTimer = 5;
+	}
+
+}
