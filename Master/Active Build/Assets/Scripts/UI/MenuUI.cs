afd16874875d23b11b23a04466a6a8a648683a1f
alexkonusa <alex.konusa@hotmail.co.uk>
2017-03-24 20:24:48 +0000

    Main Menu With live highscore board!

---
+++ Master/Active Build/Assets/Scripts/UI/MenuUI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuUI : MonoBehaviour
+{
+    public string playSceneName;
+    public string creditsSceneName;
+
+    public void PlayButton()
+    {
+
+        SceneManager.LoadScene(playSceneName);
+
+    }
+
+    public void CreditsButton()
+    {
+
+        SceneManager.LoadScene(creditsSceneName);
+
+    }
+
+    public void CloseApplication()
+    {
+
+        Application.Quit();
+
+    }
+
+}
