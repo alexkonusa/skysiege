e86a06d6031129d9ae5b1672bad67a45f282d32d
Rayner <robrayner96@hotmail.com>
2017-03-29 10:59:00 +0100

    Fix all the things with the button. I pray to got it still works

--- Master/Active Build/Assets/Scripts/Rob/ButtonSkippy.cs
+++ Master/Active Build/Assets/Scripts/Rob/ButtonSkippy.cs
@@ -4,13 +4,29 @@ using UnityEngine.UI;
 
 public class ButtonSkippy : MonoBehaviour {
 
+	public Transform Button;
+	public Transform TextPanel;
+
 	public void SkipWait () {
 		//Set Wave to be eligible to start
-		GameObject.Find ("GameManagers").GetComponent<WaveMachine> ().CheckIfThereAreNoEnemiesLeft();
+		GameObject.Find ("GameManagers").GetComponent<WaveMachine> ().CheckIfThereAreNoEnemiesLeft ();
 		// Sets the timer to 5 seconds
 		GameObject.Find ("GameManagers").GetComponent<WaveMachine> ().startTimer = 5;
-
+		//Destroys itself once pressed
+		GameObject.Destroy (GameObject.FindGameObjectWithTag("SkipWave"), 1);
 	}
 
+	void Update(){
+
+		if (GameObject.Find ("GameManagers").GetComponent<WaveManager> ().CurrentActiveEnemies.Count <= 0 && GameObject.FindGameObjectWithTag("SkipWave") == false) {
+
+			Transform ButtonSpawn = (Transform)Instantiate (Button, Button.transform.position, Quaternion.identity);
+			ButtonSpawn.transform.SetParent (TextPanel, false);
+			ButtonSpawn.transform.SetSiblingIndex (2);
+
+
+		}
+		
+	}
 
 }
